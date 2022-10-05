1f3cccf1bced9c3b3704343cac6334ea89be540e
Noman Sheikh <jitcsenomansheikh@gmail.com>
2022-10-05 23:20:16 +0530

    All Staging branch changes till now  (#4)
    * Home + calendar page added. linked to each other
    
    * switched to postgress from sqlite3
    
    * Firebase connectivity added
    
    * correction debugging
    
    * correction debugging
    
    * debugging issue
    
    * debugginf successful
    
    * stylings added
    
    * html styled

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,21 @@
+class HomeController < ApplicationController
+
+  def index
+    load_tasks
+    @tasks
+    @task_selected = @tasks[0]
+  end
+
+  def calendar
+  end
+
+  def conn
+    @conn ||= App.task_reminder_firebase.conn
+  end
+
+  def load_tasks
+    @tasks ||= conn.get("task/").body.compact
+    #binding.pry
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
+
 Rails.application.routes.draw do
+  get 'home/calendar'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  root 'home#index'
   # Defines the root path route ("/")
   # root "articles#index"
+  #get '/books' 
+  #get_ycs '/users/:status' => 'users#index', foo: 'bar'
+
 end
