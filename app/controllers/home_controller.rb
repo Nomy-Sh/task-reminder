8de4b37c3a574be31885b2fed1d057c7beddd7b4
Noman Sheikh <jitcsenomansheikh@gmail.com>
2022-10-05 20:05:52 +0530

    Firebase connectivity added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,40 +1,20 @@
 class HomeController < ApplicationController
+
+  def index
+    load_tasks
+    @tasks
+  end
+
   def calendar
   end
 
-  def create
-    resp = HTTParty.post("https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=#{ENV['API_KEY']}",
-    :body => {
-      email: user_params['email'],
-      password: user_params['password'],
-      returnSecureToken: true
-    }.to_json,
-    :headers => {'Content-Type' => 'application/json'}    
-    )
-
-    if resp.key?("idToken")
-      render json: {message: "Successfully logged in!", token: resp['idToken'], localId: resp['localId']}
-    else
-      render json: {message: "Error logging in!", error: resp["error"]}
-    end
+  def conn
+    @conn ||= App.task_reminder_firebase.conn
   end
 
-  def self.create_user(email, password)
-    resp = HTTParty.post("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=#{ENV['API_KEY']}",
-    :body => {
-      email: email,
-      password: password
-      }.to_json,
-    :headers => {'Content-Type' => 'application/json'}
-    )
-
-    if resp.key?('idToken')
-      User.create(email: resp["email"], uid: resp["localId"])
-      return {message: 'Success', token: resp['idToken']}
-    else
-      return {message: resp["error"]["errors"][0]}
-    end
+  def load_tasks
+    @tasks ||= conn.get("task/").body.compact
+    #binding.pry
   end
-
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
+class << self
+  def valid_request_exec?
+    return true if instance_methods.include?(:request_exec)
+    raise "Tried to call request_exec on a class where it is not defined, #{self}"
+  end
+
+  %i[get post patch delete put].each do |request_method|
+    define_method(:"#{request_method}_ycs") do |path, klass, opts = {}, &block|
+      send(request_method, path, opts = {}) {
+        request_exec(path, klass, opts, &block) if self.class.valid_request_exec?
+      }
+    end
+  end
+end
+
+def request_exec(_path, klass, opts = {})
+  additional_opts = yield(request) if block_given?
+  service = klass.new(params.merge(opts))
+  service.validate
+  service.execute_with_rescue
+  respond_with_code(service.respond)
+end
+
 Rails.application.routes.draw do
   get 'home/calendar'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -5,4 +28,7 @@ Rails.application.routes.draw do
   root 'home#index'
   # Defines the root path route ("/")
   # root "articles#index"
+  #get '/books' 
+  #get_ycs '/users/:status' => 'users#index', foo: 'bar'
+
 end
