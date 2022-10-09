57e5660137f03de251e85fd82b82a3604ea89bbd
Noman Sheikh <jitcsenomansheikh@gmail.com>
2022-10-09 21:16:45 +0530

    debugging

--- db/migrate/20221009145458_sample_name_change_column_type.rb
+++ db/migrate/20221009145458_sample_name_change_column_type.rb
@@ -1,5 +1,6 @@
 class SampleNameChangeColumnType < ActiveRecord::Migration[7.0]
   def change
-    #change_column :tasks, :time, 'time USING CAST(time AS time)'
+    change_column :tasks, :time, 'time USING CAST(time AS time)'
+    change_column :tasks, :date, 'date USING CAST(date AS date)'
   end
 end
